76108e81827e688405e42e50745e349628024c9e
GMSNOW <snows6005@gmail.com>
2026-06-11 18:08:56 +0300

    remove temp check file

--- _check.mjs
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const p = new PrismaClient();
-p.category.findMany({ take: 10 }).then((r) => {
-  r.forEach((c) => console.log(c.slug, c.name));
-  p.$disconnect();
-});
